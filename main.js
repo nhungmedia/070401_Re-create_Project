cdf538b28744e29df3eabc9696481b4311867d80
nhungmedia <nhun0029@stud.kea.dk>
2022-02-22 22:21:04 +0100

    Add footer style

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ ImgParent.appendChild(copy)
 const ImgGrid = document.querySelector (".imageContainer")
 const Img2Parent = document.querySelector (".imageGrid")
 //Do smth 10 times
-for(let i = 0; i < 41; i++){
+for(let i = 0; i < 4; i++){
 const copy = ImgGrid.cloneNode(true)
 Img2Parent.appendChild(copy)
 }
