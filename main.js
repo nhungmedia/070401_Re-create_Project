6fc439d3d3f49705d92ea59e91b0bccb106ea481
nhungmedia <nhun0029@stud.kea.dk>
2022-02-22 13:50:11 +0100

    style and js more

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ for(let i = 0; i < 10; i++){
 const copy = ImgWrapper.cloneNode(true)
 ImgParent.appendChild(copy)
 }
+const ImgGrid = document.querySelector (".imageContainer")
+const Img2Parent = document.querySelector (".imageGrid")
+//Do smth 10 times
+for(let i = 0; i < 41; i++){
+const copy = ImgGrid.cloneNode(true)
+Img2Parent.appendChild(copy)
+}
